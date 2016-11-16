70fb9711fc7d6e825a35c06a3d50ec093b47d10e
Raygan Henley <raygan@raygan.com>
2016-11-16 15:52:20 +0900

    Added thumbnails.  Added films section.  Connected nav widget targets.  Added URI fragment logic on page load.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,34 @@
 function toggleOpen( object ) {
 	object.style.height = "20em";
+	object.raygancom.toggleOpenState
 }
 
 function setResults ( content_html ) {
 	document.getElementById('raygancom_resultlist').innerHTML = content_html;
 }
 
+function navTo ( filepath ) {
+	var cgipath = "dirtydir.cgi?" + filepath;
+	$.get( cgipath, setResults );
+}
 
-$.get("dirtydir.cgi?fs/frontpg/", function(data){
-	document.getElementById('raygancom_resultlist').innerHTML=data;
-});
+function navToFrag ( url ) {
+	frag = "frontpg";
+	var i = url.indexOf( "#" ) + 1;
+	if( i != "" ) {
+		var frag = url.substr( i );
+	}
+	if( frag == "films" || frag == "frontpg" ) {
+	}else{
+		frag = "frontpg";
+	}
+
+	var filepath = "fs/" + frag;
+	navTo( filepath );
+}
+
+navToFrag( document.URL );
+
+
+/* $.get("dirtydir.cgi?fs/frontpg/", setResults ); */
 
